60f93c7a71e9762fa68d262c029ea1685cfbfef4
Takafumi Arakaki <devnull@localhost>
2010-10-11 23:49:13 +0200

    added README.rst, LICENSE and samples/

---
+++ README.rst
@@ -0,0 +1,26 @@
+RailGun --- Accelerate your simulation programing with "C on Rails"
+===================================================================
+
+..
+
+  Installation
+  ------------
+  ::
+
+      easy_install railgun  # using setuptools
+      pip install railgun   # using pip
+
+
+Usage
+-----
+See samples in ``samples/``.
+
+
+Requirement
+-----------
+This module only requires Numpy.
+
+
+License
+-------
+See LICENSE
---
+++ samples/lode/lode.c
@@ -0,0 +1,20 @@
+typedef struct linearode_{
+  int num_d, num_s;
+  double dt;
+  double **a;
+  double **x;
+} LinearODE;
+
+int LinearODE_run(LinearODE *self, int s_end)
+{
+  int s, d1, d2;
+  for (s = 1; s < s_end; ++s){
+    for (d1 = 0; d1 < self->num_d; ++d1){
+      self->x[s][d1] = self->x[s-1][d1];
+      for (d2 = 0; d2 < self->num_d; ++d2){
+        self->x[s][d1] += self->dt * self->a[d1][d2] * self->x[s-1][d2];
+      }
+    }
+  }
+  return 0;
+}
