518245c4ad81bdf3cbbd1f55835121e4019ab459
Takafumi Arakaki <aka.tkf@gmail.com>
2010-10-12 14:43:05 +0200

    added as_ndim_pointer for ndim c-array test_arrayaccess.py includes test for dim=3 now

--- tests/ext/src/arrayaccess.c
+++ tests/ext/src/arrayaccess.c
@@ -1,9 +1,9 @@
 typedef struct arrayaccess_{
-  int num_i, num_j;
+  int num_i, num_j, num_k;
   int ret_int;
   double ret_double;
-  int *int1d, **int2d;
-  double *double1d, **double2d;
+  int *int1d, **int2d, ***int3d;
+  double *double1d, **double2d, ***double3d;
 } ArrayAccess;
 
 int ArrayAccess_get_int1d(ArrayAccess *self, int i){
@@ -16,6 +16,11 @@ int ArrayAccess_get_int2d(ArrayAccess *self, int i, int j){
   return 0;
 }
 
+int ArrayAccess_get_int3d(ArrayAccess *self, int i, int j, int k){
+  self->ret_int = self->int3d[i][j][k];
+  return 0;
+}
+
 int ArrayAccess_get_double1d(ArrayAccess *self, int i){
   self->ret_double = self->double1d[i];
   return 0;
@@ -23,5 +28,10 @@ int ArrayAccess_get_double1d(ArrayAccess *self, int i){
 
 int ArrayAccess_get_double2d(ArrayAccess *self, int i, int j){
   self->ret_double = self->double2d[i][j];
+  return 0;
+}
+
+int ArrayAccess_get_double3d(ArrayAccess *self, int i, int j, int k){
+  self->ret_double = self->double3d[i][j][k];
   return 0;
 }
--- tests/test_arrayaccess.py
+++ tests/test_arrayaccess.py
@@ -4,13 +4,16 @@ from nose.tools import raises
 
 from railgun import SimObject, relpath
 
-LIST_IDX = list('ij')
+LIST_IDX = list('ijk')
+LIST_NUM = [3, 5, 7]
 LIST_CDT = 'int', 'double'
-LIST_DIM = [1, 2]
+LIST_DIM = [1, 2, 3]
 
 
 def get_str_get_array(cdt, dim):
     """
+    Get c-function declaration 'get_{int,double}{1,2,..}d(i, j, ...)'
+
     >>> get_str_get_array('int', 1)
     'get_int1d(i)'
     >>> get_str_get_array('int', 2)
@@ -19,6 +22,7 @@ def get_str_get_array(cdt, dim):
     'get_double1d(i)'
     >>> get_str_get_array('double', 2)
     'get_double2d(i, j)'
+
     """
     args = ', '.join(LIST_IDX[:dim])
     return 'get_%s%dd(%s)' % (cdt, dim, args)
@@ -26,14 +30,17 @@ def get_str_get_array(cdt, dim):
 
 def get_str_cddec(cdt, dim):
     """
+    Get c-data declaration '{int, double} {int,double}{1,2,..}[i][j]...'
+
     >>> get_str_cddec('int', 1)
-    'int1d[i]'
+    'int int1d[i]'
     >>> get_str_cddec('int', 2)
-    'int2d[i][j]'
+    'int int2d[i][j]'
     >>> get_str_cddec('double', 1)
-    'double1d[i]'
+    'double double1d[i]'
     >>> get_str_cddec('double', 2)
-    'double2d[i][j]'
+    'double double2d[i][j]'
+
     """
     args = ']['.join(LIST_IDX[:dim])
     return '%s %s%dd[%s]' % (cdt, cdt, dim, args)
@@ -44,7 +51,7 @@ class ArrayAccess(SimObject):
     _clibdir_ = relpath('ext/build', __file__)
 
     _cmembers_ = (
-        ['num_%s = %d' % data for data in zip(LIST_IDX, [3, 5])] +
+        ['num_%s = %d' % data for data in zip(LIST_IDX, LIST_NUM)] +
         ['int ret_int', 'double ret_double'] +
         [get_str_cddec(cdt, dim) for cdt in LIST_CDT for dim in LIST_DIM]
         )
