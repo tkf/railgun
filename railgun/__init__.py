ded773c40668d0bdeb609a3eeef92ef111b7cb71
Takafumi Arakaki <aka.tkf@gmail.com>
2010-10-12 16:37:10 +0200

    added setup.py

--- railgun/__init__.py
+++ railgun/__init__.py
@@ -1,5 +1,10 @@
+__author__  = "Takafumi Arakaki (aka.tkf@gmail.com)"
+__version__ = "0.1"
+__license__ = "MIT License"
+
 import os
 from railgun.simobj import SimObject
+
 
 def relpath(path, fpath):
     """Get path from python module by (getpath('ext/...', __file__))"""
