c0947fed167b8af922d8280acad7abc118095cca
Takafumi Arakaki <devnull@localhost>
2010-10-13 19:03:57 +0200

    added utility functions `cmint` and `cmdouble` and generator for them

--- railgun/__init__.py
+++ railgun/__init__.py
@@ -33,3 +33,34 @@ from railgun.simobj import SimObject
 def relpath(path, fpath):
     """Get path from python module by (getpath('ext/...', __file__))"""
     return os.path.join(os.path.dirname(fpath), path)
+
+
+def gene_cmem(cdt):
+    """
+    Generate function `cm{cdt}` automatically
+
+    >>> cmint = gene_cmem('int')
+    >>> cmint('a', 'b', 'c')
+    ['int a', 'int b', 'int c']
+    >>> cmint('a, b, c')
+    ['int a', 'int b', 'int c']
+
+    """
+    def cmem(*args):
+        if len(args) == 1:
+            args = args[0].split(',')
+        return ['%s %s' % (cdt, v.strip()) for v in args]
+    cmem.func_name = 'cm%s' % cdt
+    cmem.__doc__ = """
+    Generate `_cmembers_` for same C type easily
+
+    >>> cm%(cdt)s('a, b, c')
+    ['%(cdt)s a', '%(cdt)s b', '%(cdt)s c']
+    >>> cm%(cdt)s('a', 'b', 'c')
+    ['%(cdt)s a', '%(cdt)s b', '%(cdt)s c']
+
+    """ % dict(cdt=cdt)
+    return cmem
+
+cmint = gene_cmem('int')
+cmdouble = gene_cmem('double')
