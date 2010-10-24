c55eb36594f8951c10fdf39bba85414d6b95a304
Takafumi Arakaki <devnull@localhost>
2010-10-24 14:52:54 +0200

    added tests/test_arrayaccess.py

---
+++ tests/test_arrayaccess.py
@@ -0,0 +1,11 @@
+from arrayaccess import check_arrayaccess, LIST_CDT, NDIM
+
+LIST_NUM = [11, 7, 5, 3, 2]
+# LIST_NUM = [2] * NDIM
+
+
+def test_arrayaccess():
+    clibname = 'arrayaccess.so'
+    for cdt in LIST_CDT:
+        for dim in range(1, 1 + NDIM):
+            yield (check_arrayaccess, clibname, LIST_NUM, LIST_CDT, cdt, dim)
