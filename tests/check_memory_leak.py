e85cecaf9a5dcbf6667348e75ab628e38bc9921d
Takafumi Arakaki <aka.tkf@gmail.com>
2010-11-26 15:06:56 +0100

    enable --calloc (-c) option of check_memory_leak.py

--- tests/check_memory_leak.py
+++ tests/check_memory_leak.py
@@ -17,7 +17,7 @@ def main(iter_num, list_num, calloc):
             0, iter_num, num=10, endpoint=False).astype(int)
 
     num_dict = dict(zip(ArrayAccess.num_names, list_num))  # {num_i: 6, ...}
-    assert ArrayAccess._calloc_ is True
+    assert ArrayAccess._calloc_ is bool(calloc)
 
     print '[*/%d]:' % iter_num,
     sys.stdout.flush()
@@ -34,7 +34,7 @@ if __name__ == '__main__':
     parser.add_option("-n", "--nums", default='100, 10, 10, 10, 10',
                       help="comma separated numbers")
     parser.add_option("-t", "--time", default=1000, type=int)
-    parser.add_option("-c", "--calloc", default=1)
+    parser.add_option("-c", "--calloc", default=1, type=int)
     (opts, args) = parser.parse_args()
     if opts.nums:
         list_num = eval('[%s]' % opts.nums)
