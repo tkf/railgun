bc7b65304460a144ae5f270fbc7c88f8c62684ca
Takafumi Arakaki <aka.tkf@gmail.com>
2013-04-13 21:42:25 +0200

    Remove tools/railgun-ci.sh

--- tools/railgun-ci.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-python setup.py build_ext -i
-make --directory=tests/ext/
-nosetests tests --with-xunit
-export PYTHONPATH="`pwd`:$PYTHONPATH"
-echo "PYTHONPATH=$PYTHONPATH"
-cd doc/ && make clean && make html
